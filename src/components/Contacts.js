729ffc17643f8f47f9ac36774722d8be4ae231d8
faysal_bsa <faysal.ce@hotmail.com>
2019-09-05 20:09:15 +0600

    filter basic included

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,7 +1,9 @@
 import React, { Component, Fragment } from 'react';
-import { Layout, List, Avatar, Button, Skeleton, Row, Col, Tabs } from 'antd';
+import { Layout, List, Avatar, Button, Skeleton, Row, Col, Tabs, Menu, Icon, Radio, Input } from 'antd';
 import reqwest from 'reqwest';
+import './style.css'
 const { Header, Content } = Layout;
+const { Search } = Input;
 const { TabPane } = Tabs;
 
 const count = 5;
@@ -73,9 +75,10 @@ class Contacts extends Component {
                         marginTop: 12,
                         height: 32,
                         lineHeight: '32px',
+                        marginBottom:'24px'
                     }}
                 >
-                    <Button onClick={this.onLoadMore}>loading more</Button>
+                    <Button  onClick={this.onLoadMore}>loading more</Button>
                 </div>
             ) : null;
 
@@ -83,70 +86,103 @@ class Contacts extends Component {
         return (
             <Fragment>
                 <Header style={{ background: '#002766' }}>
-                    <div className="logo"  >
-                        <img style={{ maxWidth: '120px' }} src="https://www.leadbook.com/wp-content/uploads/2018/06/leadbook-logo-light-hi-res.png" alt="Lead book" />
+                    <div className="logo" style={{ float: 'left', width: '120px' }} >
+                        <img style={{ maxWidth: '100%' }} src="https://www.leadbook.com/wp-content/uploads/2018/06/leadbook-logo-light-hi-res.png" alt="Lead book" />
                     </div>
+                    <Menu
+                        theme="dark"
+                        mode="horizontal"
+                        defaultSelectedKeys={['2']}
+                        style={{ background: '#002766', lineHeight: '64px', float: 'right' }}
+                    >
 
+                        <Menu.Item key="3"><Icon type="logout" />Logout</Menu.Item>
+                    </Menu>
                 </Header>
                 <Content style={{ padding: '50px 50px 0' }}>
 
-                    <div style={{minHeight: "calc(100vh - 183px)" }}>
+                    <div style={{ minHeight: "calc(100vh - 183px)" }}>
+
+
                         <Row type="flex" justify="center" align="middle">
-                            <Col span={8}>
-<div style={{backgroundColor:'#fff', padding:'50px', paddingLeft:'5px'}} >
-<Tabs defaultActiveKey="1" tabPosition='left' >
-                                    <TabPane tab="All Contacts" key="1">
-                                        <List
-                                            className="demo-loadmore-list"
-                                            loading={initLoading}
-                                            itemLayout="horizontal"
-                                            loadMore={loadMore}
-                                            dataSource={list}
-                                            renderItem={item => (
-                                                <List.Item
-                                                    actions={[<a key="list-loadmore-edit">edit</a>, <a key="list-loadmore-more">more</a>]}
-                                                >
-                                                    <Skeleton avatar title={false} loading={item.loading} active>
-                                                        <List.Item.Meta
-                                                            avatar={
-                                                                <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
-                                                            }
-                                                            title={<a href="https://ant.design">{item.name.last}</a>}
-                                                            description="Ant Design, a design language for background applications, is refined by Ant UED Team"
-                                                        />
+                            <Col span={12}>
+                                <div style={{ backgroundColor: '#fff' }} >
+                                    <div style={{ padding: '24px', borderBottom: '1px solid #e8e8e8' }}>
+                                        <Row type="flex" justify="end" align="middle">
+                                            <Col>
+                                                <Radio.Group defaultValue="a" buttonStyle="solid">
+                                                    <Radio.Button value="a">Hangzhou</Radio.Button>
+                                                    <Radio.Button value="b">Shanghai</Radio.Button>
+                                                    <Radio.Button value="c">Beijing</Radio.Button>
+                                                    <Radio.Button value="d">Chengdu</Radio.Button>
+                                                </Radio.Group>
+                                            </Col>
+                                            <Col>
+                                                <Search
+                                                    placeholder="input search text"
+                                                    onSearch={value => console.log(value)}
+                                                    style={{ width: 200, marginLeft:'8px' }}
+                                                />
+                                            </Col>
 
-                                                    </Skeleton>
-                                                </List.Item>
-                                            )}
-                                        />
-                                    </TabPane>
-                                    <TabPane tab="Fev Contacts" key="2">
-                                        <List
-                                            className="demo-loadmore-list"
-                                            loading={initLoading}
-                                            itemLayout="horizontal"
-                                            loadMore={loadMore}
-                                            dataSource={list}
-                                            renderItem={item => (
-                                                <List.Item
-                                                    actions={[<a key="list-loadmore-edit">edit</a>, <a key="list-loadmore-more">more</a>]}
-                                                >
-                                                    <Skeleton avatar title={false} loading={item.loading} active>
-                                                        <List.Item.Meta
+                                        </Row>
+                                    </div>
+                                    <div>
+                                        <Tabs defaultActiveKey="1" tabPosition='left' >
+                                            <TabPane tab="All Contacts" key="1">
+                                                <List
+                                                    className="demo-loadmore-list"
+                                                    loading={initLoading}
+                                                    itemLayout="horizontal"
+                                                    loadMore={loadMore}
+                                                    dataSource={list}
+                                                    renderItem={item => (
+                                                        <List.Item
+                                                            actions={[<a key="list-loadmore-edit"><Icon type="heart" /></a>, <a key="list-loadmore-more"> <Icon type="heart" theme="filled" /></a>]}
+                                                        >
+                                                            <Skeleton avatar title={false} loading={item.loading} active>
+                                                                <List.Item.Meta
+                                                                    avatar={
+                                                                        <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+                                                                    }
+                                                                    title={<a href="https://ant.design">{item.name.last}</a>}
+                                                                    description="Ant Design, a design language for background applications, is refined by Ant UED Team"
+                                                                />
 
-                                                            title={<a href="https://ant.design">{item.name.last}</a>}
-                                                            description="Ant Design, a design language for background applications, is refined by Ant UED Team"
-                                                        />
+                                                            </Skeleton>
+                                                        </List.Item>
+                                                    )}
+                                                />
+                                            </TabPane>
+                                            <TabPane tab="Fev Contacts" key="2">
+                                                <List
+                                                    className="demo-loadmore-list"
+                                                    loading={initLoading}
+                                                    itemLayout="horizontal"
+                                                    loadMore={loadMore}
+                                                    dataSource={list}
+                                                    renderItem={item => (
+                                                        <List.Item
+                                                            actions={[<a key="list-loadmore-edit"><Icon type="heart" /></a>, <a key="list-loadmore-more"> <Icon type="heart" theme="filled" /></a>]}
+                                                        >
+                                                            <Skeleton avatar title={false} loading={item.loading} active>
+                                                                <List.Item.Meta
 
-                                                    </Skeleton>
-                                                </List.Item>
-                                            )}
-                                        />
-                                    </TabPane>
+                                                                    title={<a href="https://ant.design">{item.name.last}</a>}
+                                                                    description="Ant Design, a design language for background applications, is refined by Ant UED Team"
+                                                                />
 
-                                </Tabs>
-</div>
-                                
+                                                            </Skeleton>
+                                                        </List.Item>
+                                                    )}
+                                                />
+                                            </TabPane>
+
+                                        </Tabs>
+                                    </div>
+
+                                </div>
+
 
                             </Col>
                         </Row>
